b9cc1d7b3739167cc3563303c6db3ad621a194c9
edeustace <ed.eustace@gmail.com>
2015-12-23 16:50:58 +0000

    add build-from-git cmd

--- lib/cs-builder/cmds/build-from-git.rb
+++ lib/cs-builder/cmds/build-from-git.rb
@@ -1,22 +1,29 @@
 require_relative 'core-command'
 require_relative '../models/paths'
+require_relative '../git/repo'
+require_relative '../runner'
 require_relative '../git/git-parser'
 
 
 include CsBuilder::Git 
 
 module CsBuilder
-  module Cmds
+  module Commands
     class BuildFromGit < CoreCommand
 
+      include CsBuilder::Runner
+      
       def initialize(config_dir)
         super('build-from-git', config_dir)
       end
 
       def run(git_url:, branch:, cmd:, org: nil, repo_name: nil)
-        org = org.nil? ? GitUrlParser.org(git_url) : org 
-        repo_name = repo_name.nil? ? GitUrlParser.org(git_url) :  repo_name 
+        org = nil_or_empty?(org) ? GitUrlParser.org(git_url) : org 
+        repo_name = nil_or_empty?(repo_name) ? GitUrlParser.org(git_url) : repo_name 
         @paths = Paths.new(@config_dir, org, repo_name, branch)
+        repo = Repo.new(@config_dir, git_url, org, repo_name, branch)
+        repo.clone_and_update
+
         run_with_lock(@paths.lock_file("build")) {
           in_dir(@paths.repo){
             @log.debug( "run: #{cmd}")
@@ -24,6 +31,11 @@ module CsBuilder
           }
         }
       end
+
+      def nil_or_empty?(s)
+        s.nil? or s.empty?
+      end
+
     end
   end
 end
