5de5f00170941836f655085d2ab5ec6417728947
edeustace <ed.eustace@gmail.com>
2016-01-22 14:13:19 +0000

    update cli spec

--- spec/unit/cli_spec.rb
+++ spec/unit/cli_spec.rb
@@ -4,15 +4,42 @@ describe CLI do
   def mk_stub(n)
     d = double 
     stub_const(n, d)
-    d.stub(:new).with(String){ |s| d } 
-    d.stub(:run).with(anything()){|opts| opts}
+    d.stub(:new).with(anything(), anything()){ |s| d } 
+    d.stub(:run).with(anything()){|opts| 
+      opts
+    }
+    d
+  end
+  
+
+  describe "artifact-list" do 
+    it "call :run" do 
+      double = mk_stub("CsBuilder::Commands::Artifacts::List")
+      out = CsBuilder::CLI.start(%w[artifact-list --org=org --repo=repo])
+      double.should have_received(:run).with(
+        {
+          :org => "org", 
+          :repo => "repo"
+        })
+    end
   end
 
   describe "mk-artifact-from-git" do 
 
     it "call :run" do 
-      mk_stub("CsBuilder::Commands::Artifacts::MkFromGit")
-      CsBuilder::CLI.start(%w[artifact-mk-from-git --cmd="build" --branch=b --artifact-pattern="p" --git=git@github.com:blah/blah.git])
+      double = mk_stub("CsBuilder::Commands::Artifacts::MkFromGit")
+      args = %w[artifact-mk-from-git --cmd=cmd --branch=branch --artifact=artifact --git=git]
+      lambda { CsBuilder::CLI.start(args) }.should_not raise_error
+      double.should have_received(:run).with(
+        {
+          :git_url => "git", 
+          :branch => "branch",
+          :cmd => "cmd",
+          :artifact => "artifact",
+          :org => nil,
+          :repo_name => nil,
+          :force => false
+        })
     end
   end
 
